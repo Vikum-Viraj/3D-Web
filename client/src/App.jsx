182e8959c2b5576cac5510b6ea2782943f99376c
Vikum Viraj <Mvikumviraj@gmail.com>
2023-06-29 14:06:56 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import Customizer from './pages/Customizer'
+import Canvas from './canvas'
+import state from './store'
+
+
+function App() {
+
+
+  return (
+    <main className='app transition-all ease-in'>
+      <Home/>
+      <Canvas/>
+      <Customizer/>
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/components/AIpicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AIpicker = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AIpicker
